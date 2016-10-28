61bd2c2de5554b4ccf400a473e73e1bbf78ecce5
Gloorf <glorf@glorf.fr>
2016-10-28 16:00:14 +0200

    Added licence information (AGPL)

--- inventory.py
+++ inventory.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python3
-from flask import Blueprint, render_template, redirect, url_for, request
+# Copyright (C) 2016 Guillaume Dupuy
+# Author: Guillaume Dupuy
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
+from flask import Blueprint, render_template, redirect, url_for, request
 import flask_babel
 
 from ryzomapi import APIKey
